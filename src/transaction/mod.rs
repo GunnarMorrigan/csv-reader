1f218d673a9a97e424fa71efcfecfdd288042b0d
Gunnar <gunnar@MacBook-Pro.local>
2024-12-14 10:05:35 +0100

    feat: add deserialization of csv row item

--- src/transaction/mod.rs
+++ src/transaction/mod.rs
@@ -1,9 +1,34 @@
+use rust_decimal::Decimal;
+use serde::{Deserialize, Serialize};
 pub mod charge_back;
 pub mod deposit;
 pub mod dispute;
 pub mod error;
 pub mod resolve;
 pub mod withdrawal;
+
+pub fn transaction_reader<R>(reader: R) -> csv::DeserializeRecordsIntoIter<R, TransactionRow>
+where
+    R: std::io::Read,
+{
+    let rdr = csv::ReaderBuilder::new()
+        .flexible(true)
+        .has_headers(true)
+        .trim(csv::Trim::All)
+        .from_reader(reader);
+    rdr.into_deserialize()
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug, Serialize, Deserialize)]
+pub struct TransactionRow {
+    #[serde(rename = "type")]
+    transaction_type: TransactionType,
+    client: Client,
+    #[serde(rename = "tx")]
+    transaction_id: TransactionId,
+    amount: Option<Decimal>,
+}
+
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug, Serialize, Deserialize)]
 pub enum TransactionType {
     #[serde(rename = "deposit")]
